1b634fe40f43ec5a9af7721e4dc1ce621f0e646d
noor-alkawarit <nooralkawarit@gmail.com>
2025-10-16 18:25:49 +0100

    started creating to do list

---
+++ main.js
@@ -0,0 +1,39 @@
+const ulElement = document.getElementById("list");
+const body = document.getElementsByTagName("body").item(0); //<body>append</body>
+const input = document.getElementById("listInput")
+
+//Press button => runs this function
+function addItem () {
+
+  //get input value
+  const data = input.value.trim();
+
+  //check if input value == ""
+  if(data == ""){
+    //input value is empty, exit function
+    return;
+  }
+
+  //check if data is empty?
+ 
+
+  const li = document.createElement("li")
+  const text = document.createElement("p")
+  text.innerText = data
+  const button = document.createElement("button")
+  button.addEventListener ("click", (e) => deleteItem(e))
+  button.innerText = "delete"
+
+  //append text to li
+  li.appendChild(text)
+  //append button to li
+ li.appendChild(button)
+
+  ulElement.appendChild(li)
+  // reset input field
+  input.value = ""
+}
+
+function deleteItem(event){
+  //runs when we press delete button on Li
+}
